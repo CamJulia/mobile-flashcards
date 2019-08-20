9b16b173ddf8befb1abb9e4ca2791f2719b912e6
Julia Çam <barbarbetsy@googlemail.com>
2019-08-20 21:30:26 +0200

    add add card file

---
+++ screens/AddCard.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  View
+} from 'react-native';
+import { getDeck, addCardToDeck } from '../src/storage';
+
+export default class AddCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      deck: null,
+      title: '',
+      body: ''
+    };
+  }
+
+  async componentDidMount() {
+    const { params } = this.props.navigation.state;
+    const deck = await getDeck(params.title);
+    this.setState({ deck });
+  }
+
+  render() {
+    const { deck, title, body } = this.state;
+
+    if (!deck) return null;
+
+    return (
+      <View style={{ margin: 20 }}>
+        <Text>This is where we'll create new cards</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={title => this.setState({ title })}
+          value={title}
+          placeholder="enter card title"
+        />
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={body => this.setState({ body })}
+          value={body}
+          placeholder="enter card body"
+        />
+        <Button
+          onPress={() => {
+            addCardToDeck(deck.title, { title, body });
+            this.setState({ title: '', body: '' });
+          }}
+          title="create card"
+        />
+      </View>
+    );
+  }
+}
+
+AddCard.navigationOptions = () => ({ title: 'Add Card to Deck' });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green'
+  }
+});
